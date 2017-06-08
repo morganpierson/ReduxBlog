0a7c696f6c33af627d7d904bc3858f086df0e7ff
Morgan Pierson <morganpierson@gmail.com>
2017-06-08 16:56:45 -0700

    redux-form added, basic form component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise'
-import { BrowserRouter, Route } from 'react-router-dom'
-import PostsIndex from './components/postsIndex';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import reducers from './reducers';
+import PostsIndex from './components/postsIndex';
+import PostsNew from './components/postsNew';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -26,7 +27,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter> 
       <div>
+      <Switch>
+        <Route path='/posts/new' component={PostsNew} />
         <Route path='/' component={PostsIndex} />
+      </Switch>
       </div>
     </BrowserRouter>
 

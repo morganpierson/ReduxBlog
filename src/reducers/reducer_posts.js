f01629c196496ea3a152e5597b9d956777a05259
Morgan Pierson <morganpierson@gmail.com>
2017-06-09 16:30:40 -0700

    posts show component

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,18 @@
-import { FETCH_POSTS } from '../actions'
+import { FETCH_POSTS, FETCH_POST } from '../actions'
 import _ from 'lodash';
+
 
 export default function (state = {}, action) {
   switch(action.type) {
+    case FETCH_POST:
+      const post = action.payload.data;
+      // const newState =  { ...state, id: action.payload.data };
+      // newState[post.id] = post;
+      // return newState;
+      //Code below is same as code above only using ES6 syntax
+      return { ...state, [action.payload.data.id]: action.payload.data }
     case FETCH_POSTS:
-      return _.mapKeys(action.payload.data, 'id')
+      return _.mapKeys(action.payload.data, 'id');
     default:
       return state
   }
